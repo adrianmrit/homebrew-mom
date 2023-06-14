9dd618c167f4d26c44ee9fce4bf6c8825bdc46b0
Adrian Martinez Rodriguez <adrianmrit@gmail.com>
2023-06-14 19:06:05 -0400

    build: release v1.3.0

--- Formula/mom.rb
+++ Formula/mom.rb
@@ -1,21 +1,21 @@
 class Mom < Formula
   desc "A task runner for teams and individuals."
   homepage "https://github.com/adrianmrit/mom#readme"
-  version "1.2.0"
+  version "1.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/adrianmrit/mom/releases/download/v1.2.0/mom-v1.2.0-aarch64-apple-darwin.zip"
-      sha256 "0acc032530203db503e139a796ed83ebf0fcb4562066308aca70ca42c2569238"
+      url "https://github.com/adrianmrit/mom/releases/download/v1.3.0/mom-v1.3.0-aarch64-apple-darwin.zip"
+      sha256 "5bac69109f2b7e3e29e98e181337807067f69cb1aa86c964b72015cce56b31ff"
 
       def install
         bin.install "mom"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/adrianmrit/mom/releases/download/v1.2.0/mom-v1.2.0-x86_64-apple-darwin.zip"
-      sha256 "7df29c3c3ebf12011ea9d673ff4c58c665b3e098e2f274598a186d354f33061d"
+      url "https://github.com/adrianmrit/mom/releases/download/v1.3.0/mom-v1.3.0-x86_64-apple-darwin.zip"
+      sha256 "66c06767a799722b8da8b477ae4fdacb81354ba6878b3a70319be637bff3e15a"
 
       def install
         bin.install "mom"
@@ -25,8 +25,8 @@ class Mom < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/adrianmrit/mom/releases/download/v1.2.0/mom-v1.2.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "ff4c27294dea2d6bf96e780abc7618495fab411fd5d2d972654ef2153c79fb95"
+      url "https://github.com/adrianmrit/mom/releases/download/v1.3.0/mom-v1.3.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "70d433417f362dd28cbc8658bc0e2041ea4e08013b3eb1e829328b98c391d992"
 
       def install
         bin.install "mom"
@@ -35,6 +35,6 @@ class Mom < Formula
   end
 
   test do
-    assert_match "mom-task 1.2.0", shell_output("#{bin}/mom --version")
+    assert_match "mom-task 1.3.0", shell_output("#{bin}/mom --version")
   end
 end
